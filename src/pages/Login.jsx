0a83d664514f0aadb07f333b1d9217d02a1fa254
henriClimateSolutions <henri@cliamte-solutions.net>
2025-10-24 11:28:17 +0200

    removed little error

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,7 +8,7 @@ export const FormField = ({ type, field }) => {
 	return (
 		<div className="w-full flex flex-col items-start gap-1">
 			<h3 >{field}:</h3>
-			<input autoComplete='new-password' type={type} className="field-input bg-amber-50 text-black" />
+			<input type={type} className="field-input bg-amber-50 text-black" />
 		</div>
 	)
 }
